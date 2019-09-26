6701ff18b1bc88385eaeca095cda45970204358f
Serhii Volchetskyi <serhii.volchetskyi@gmail.com>
2019-09-27 01:01:06 +0300

    implemented Unit of Work pattern

---
+++ LinkContractor/LinkContractor.DAL/IUnitOfWork.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using LinkContractor.DAL.Repositories;
+
+namespace LinkContractor.DAL
+{
+    public interface IUnitOfWork : IDisposable
+    {
+        ISavedDataRepository SavedData { get; }
+        IShortCodeRepository ShortCodes { get; }
+
+        int SaveChanges();
+    }
+}
