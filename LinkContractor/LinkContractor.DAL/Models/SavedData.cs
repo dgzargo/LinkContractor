5bd0f3ce03c5fb1fcb9f53d23a44139fd0fafebe
Serhii Volchetskyi <serhii.volchetskyi@gmail.com>
2019-09-26 17:03:44 +0300

    added db models and DbContext for DAL

---
+++ LinkContractor/LinkContractor.DAL/Models/SavedData.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace LinkContractor.DAL.Models
+{
+    public class SavedData
+    {
+        public int Id { get; set; }
+        public Guid Guid { get; set; }
+        public string Message { get; set; }
+        public bool IsLink { get; set; }
+        public Guid? User { get; set; }
+        public DateTime? Created { get; set; }
+        public int? TimeLimit { get; set; }
+        public DateTime? EndTime { get; set; }
+        public int? ClickLimit { get; set; }
+        public Guid? Group { get; set; }
+
+        public virtual ShortCode ShortCode { get; set; }
+    }
+}
