52f711728f3c47e3095c6c521c64f93ac6529bcc
Serhii Volchetskyi <serhii.volchetskyi@gmail.com>
2019-10-01 15:08:22 +0300

    create business logic layer classes

---
+++ LinkContractor/LinkContractor.BLL/DTO/SavedDataDTOHelper.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using LinkContractor.BLL.Message;
+using LinkContractor.DAL.Models;
+
+namespace LinkContractor.BLL.DTO
+{
+    internal static class SavedDataDTOHelper
+    {
+        public static SavedDataDTO ToDTO(this SavedData savedData)
+        {
+            var savedDataDTO = new SavedDataDTO
+            {
+                Guid = savedData.Guid,
+                Message = savedData.Message,
+                IsLink = savedData.IsLink,
+                User = savedData.User,
+                Created = savedData.Created,
+                TimeLimit = savedData.TimeLimit,
+                EndTime = savedData.EndTime,
+                ClickLimit = savedData.ClickLimit,
+                Group = savedData.Group,
+                Code = savedData.ShortCode?.Code
+            };
+            return savedDataDTO;
+        }
+
+        public static SavedData ToSavedData(this SavedDataDTO savedDataDTO)
+        {
+            var savedData = new SavedData
+            {
+                Guid = savedDataDTO.Guid,
+                Message = savedDataDTO.Message,
+                IsLink = savedDataDTO.IsLink,
+                User = savedDataDTO.User,
+                Created = savedDataDTO.Created,
+                TimeLimit = savedDataDTO.TimeLimit,
+                EndTime = savedDataDTO.EndTime,
+                ClickLimit = savedDataDTO.ClickLimit,
+                Group = savedDataDTO.Group
+            };
+            return savedData;
+        }
+
+        public static ShortCode ToShortCode(this SavedDataDTO savedDataDTO)
+        {
+            var shortCode = new ShortCode
+            {
+                RelatedGuid = savedDataDTO.Guid
+            };
+            return shortCode;
+        }
+
+        public static bool IsRecordNotExpired(this SavedData savedData)
+        {
+            return savedData.EndTime is null || savedData.EndTime > DateTime.UtcNow;
+        }
+
+        public static bool DecrementClick(this SavedData savedData)
+        {
+            if (savedData.ClickLimit is null) return true;
+            if (savedData.ClickLimit <= 0) return false;
+
+            savedData.ClickLimit--;
+            return true;
+        }
+
+        public static IMessage GetMessage(this SavedData savedData)
+        {
+            return LinkMessage.TryCreateLinkMessageIfNeeded(savedData.Message, savedData.IsLink);
+        }
+
+        public static void SwitchContent(SavedDataDTO first, SavedData second)
+        {
+            (first.Message, second.Message) = (second.Message, first.Message);
+            (first.IsLink, second.IsLink) = (second.IsLink, first.IsLink);
+            (first.ClickLimit, second.ClickLimit) = (second.ClickLimit, first.ClickLimit);
+            (first.TimeLimit, second.TimeLimit) = (second.TimeLimit, first.TimeLimit);
+        }
+
+        public static void SendToHistory(this SavedDataDTO savedDataDTO)
+        {
+            savedDataDTO.TimeLimit = null;
+            savedDataDTO.ClickLimit = -1;
+        }
+    }
+}
---
+++ LinkContractor/LinkContractor.BLL/ShortCodeTransform.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace LinkContractor.BLL
+{
+    public static class ShortCodeTransform
+    {
+        private static readonly IReadOnlyList<char> Chars;
+
+        static ShortCodeTransform()
+        {
+            Chars = ConfigureList().ToArray();
+#warning operations without overflow check
+        }
+
+        public static int? ConvertToInt(this string code)
+        {
+            var lth = Chars.Count;
+            var stack = new Stack<char>(code);
+
+            var result = 0;
+            while (stack.Count > 0)
+            {
+                result *= lth;
+                var top = stack.Pop();
+                var i = Chars.GetIndex(top);
+                if (i is null) return null;
+                result += i.Value + 1;
+            }
+
+            return result;
+        }
+
+        public static string Convert(this int code)
+        {
+            var lth = Chars.Count;
+            var list = new List<char>(16);
+            while (code > 0)
+            {
+                var mod = code % lth;
+                code /= lth;
+
+                list.Add(Chars[mod - 1]);
+            }
+
+            return new string(list.ToArray()); //without reverse
+        }
+
+        public static string Convert(this Guid code)
+        {
+            return code.ToString();
+        }
+
+        public static Guid? ConvertToGuid(this string code)
+        {
+            var successful = Guid.TryParse(code, out var result);
+            return successful ? (Guid?) result : null;
+        }
+
+        private static IEnumerable<char> ConfigureList()
+        {
+            var result = CharRange('a', 'z')
+                .Concat(CharRange('A', 'Z'))
+                .Concat(CharRange('1', '9'));
+
+            result = result.ExcludeChar('l')
+                .ExcludeChar('I')
+                .ExcludeChar('O');
+
+            return result;
+        }
+
+        private static IEnumerable<char> CharRange(char first, char last)
+        {
+            return Enumerable.Range(first, last - first + 1).Select(i => (char) i);
+        }
+
+        private static IEnumerable<char> ExcludeChar(this IEnumerable<char> en, char ch)
+        {
+            return en.Where(i => i != ch);
+        }
+
+        private static int? GetIndex(this IReadOnlyList<char> array, char of)
+        {
+            for (var i = 0; i < array.Count; i++)
+                if (array[i] == of)
+                    return i;
+            return null;
+        }
+
+        public static bool IsShortCode(this string code)
+        {
+            return code.Length < 7;
+        }
+    }
+}
